19bf3c7863d9646b2431318fc591477072370c48
Louise Poubel <louise@openrobotics.org>
2019-03-13 19:25:52 -0700

    Revert "Trimmed the branch to only the gazebo_ros_pkgs sub directory"
    This reverts commit 2da1fd4f0539b6265966b613b290e611f9236df2.

---
+++ gazebo_ros/src/gazebo_ros_init.cpp
@@ -0,0 +1,100 @@
+// Copyright 2018 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "gazebo_ros/gazebo_ros_init.hpp"
+
+#include <gazebo/common/Plugin.hh>
+#include <gazebo_ros/conversions/builtin_interfaces.hpp>
+#include <gazebo_ros/node.hpp>
+#include <gazebo_ros/utils.hpp>
+#include <rosgraph_msgs/msg/clock.hpp>
+
+#include <memory>
+
+namespace gazebo_ros
+{
+
+class GazeboRosInitPrivate
+{
+public:
+  GazeboRosInitPrivate();
+  gazebo_ros::Node::SharedPtr ros_node_;
+  rclcpp::Publisher<rosgraph_msgs::msg::Clock>::SharedPtr clock_pub_;
+  gazebo::event::ConnectionPtr world_update_event_;
+  gazebo_ros::Throttler throttler_;
+  static constexpr double DEFAULT_PUBLISH_FREQUENCY = 10.;
+
+  void PublishSimTime(const gazebo::common::UpdateInfo & _info);
+};
+
+GazeboRosInit::GazeboRosInit()
+: impl_(std::make_unique<GazeboRosInitPrivate>())
+{
+}
+
+GazeboRosInit::~GazeboRosInit()
+{
+}
+
+void GazeboRosInit::Load(int argc, char ** argv)
+{
+  // Initialize ROS with arguments
+  rclcpp::init(argc, argv);
+
+  impl_->ros_node_ = gazebo_ros::Node::Get();
+
+  // Offer transient local durability on the clock topic so that if publishing is infrequent (e.g.
+  // the simulation is paused), late subscribers can receive the previously published message(s).
+  rmw_qos_profile_t qos_profile = rmw_qos_profile_default;
+  qos_profile.durability = RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL;
+  impl_->clock_pub_ = impl_->ros_node_->create_publisher<rosgraph_msgs::msg::Clock>(
+    "/clock",
+    qos_profile);
+
+  // Get publish rate from parameter if set
+  rclcpp::Parameter rate_param;
+  if (impl_->ros_node_->get_parameter("publish_rate", rate_param)) {
+    if (rclcpp::ParameterType::PARAMETER_DOUBLE == rate_param.get_type()) {
+      impl_->throttler_ = Throttler(rate_param.as_double());
+    } else if (rclcpp::ParameterType::PARAMETER_INTEGER == rate_param.get_type()) {
+      impl_->throttler_ = Throttler(rate_param.as_int());
+    } else {
+      RCLCPP_WARN(impl_->ros_node_->get_logger(),
+        "Could not read value of param publish_rate [%s] as double/int, using default %ghz.",
+        rate_param.value_to_string().c_str(),
+        GazeboRosInitPrivate::DEFAULT_PUBLISH_FREQUENCY);
+    }
+  }
+
+  impl_->world_update_event_ = gazebo::event::Events::ConnectWorldUpdateBegin(
+    std::bind(&GazeboRosInitPrivate::PublishSimTime, impl_.get(), std::placeholders::_1));
+}
+
+GazeboRosInitPrivate::GazeboRosInitPrivate()
+: throttler_(DEFAULT_PUBLISH_FREQUENCY)
+{
+}
+
+void GazeboRosInitPrivate::PublishSimTime(const gazebo::common::UpdateInfo & _info)
+{
+  if (!throttler_.IsReady(_info.realTime)) {return;}
+
+  rosgraph_msgs::msg::Clock clock;
+  clock.clock = gazebo_ros::Convert<builtin_interfaces::msg::Time>(_info.simTime);
+  clock_pub_->publish(clock);
+}
+
+GZ_REGISTER_SYSTEM_PLUGIN(GazeboRosInit)
+
+}  // namespace gazebo_ros
---
+++ gazebo_ros/test/test_sim_time.cpp
@@ -0,0 +1,97 @@
+// Copyright 2018 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+#include <gazebo_ros/testing_utils.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rosgraph_msgs/msg/clock.hpp>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+/// Tests the simtime published to /clock by gazebo_ros_init
+class TestSimTime : public ::testing::Test
+{
+public:
+  TestSimTime() {}
+  void SetUp() override;
+  void TearDown() override;
+
+protected:
+  std::unique_ptr<gazebo_ros::GazeboProcess> gazebo_process_;
+};
+
+void TestSimTime::SetUp()
+{
+  gazebo_process_ = std::make_unique<gazebo_ros::GazeboProcess>(std::vector<const char *>{"-s",
+        "libgazebo_ros_init.so"});
+  ASSERT_GT(gazebo_process_->Run(), 0);
+}
+
+void TestSimTime::TearDown()
+{
+  ASSERT_GE(gazebo_process_->Terminate(), 0);
+  gazebo_process_.reset();
+}
+
+TEST_F(TestSimTime, TestClock)
+{
+  using ClockMsg = rosgraph_msgs::msg::Clock;
+
+  // Create a node which will use sim time
+  auto context = rclcpp::contexts::default_context::get_global_default_context();
+  std::vector<std::string> args;
+  std::vector<rclcpp::Parameter> params;
+  auto node = std::make_shared<rclcpp::Node>("test_sim_time", "", context, args, params);
+  params = {rclcpp::Parameter("use_sim_time", true)};
+
+  // TODO(louise) Setting param after node is created, and discarding first message, due to
+  // https://github.com/ros2/rclcpp/issues/595 : remove once issue is fixed.
+  node->set_parameters(params);
+
+  auto zero_msg =
+    gazebo_ros::get_message_or_timeout<ClockMsg>(node, "/clock", rclcpp::Duration(5, 0));
+  ASSERT_NE(zero_msg, nullptr);
+
+  // Get two clock messages, caching the ROS time once each is received
+  auto first_msg =
+    gazebo_ros::get_message_or_timeout<ClockMsg>(node, "/clock", rclcpp::Duration(5, 0));
+  ASSERT_NE(first_msg, nullptr);
+  auto first_msg_time = node->now();
+  auto second_msg =
+    gazebo_ros::get_message_or_timeout<ClockMsg>(node, "/clock", rclcpp::Duration(1, 0));
+  ASSERT_NE(second_msg, nullptr);
+  auto second_msg_time = node->now();
+
+  // The SIM time should be close to zero (start of simulation)
+  EXPECT_LT(rclcpp::Time(first_msg->clock), rclcpp::Time(1, 0, RCL_ROS_TIME));
+  EXPECT_LT(rclcpp::Time(second_msg->clock), rclcpp::Time(1, 0, RCL_ROS_TIME));
+  // Time should go forward
+  EXPECT_GT(rclcpp::Time(second_msg->clock), rclcpp::Time(first_msg->clock)) <<
+    rclcpp::Time(second_msg->clock).nanoseconds() << "    " <<
+    rclcpp::Time(first_msg->clock).nanoseconds();
+  // The message from the clock should be the node's time
+  EXPECT_EQ(first_msg_time, rclcpp::Time(first_msg->clock)) << first_msg_time.nanoseconds() <<
+    "    " << rclcpp::Time(first_msg->clock).nanoseconds();
+  EXPECT_EQ(second_msg_time, rclcpp::Time(second_msg->clock)) << second_msg_time.nanoseconds() <<
+    "    " << rclcpp::Time(second_msg->clock).nanoseconds();
+}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
